5aa007092200ea0d53e24f48b366fb89b07ed5f0
tatukare <tatukare@gmail.com>
2024-03-02 14:00:29 -0500

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -126,6 +126,8 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = () => console.log('Welcome to the application!!');
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -134,5 +136,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
