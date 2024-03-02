c81d3a981e922e70181d0dec06b8c42d8b34c974
tatukare <tatukare@gmail.com>
2024-03-02 13:42:05 -0500

    Initial Setup

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '3b98cad5-f7d5-4e57-9bcb-5ef03bf3f313';
+export const MODAL_CLOSE_SEC = 2.5;
